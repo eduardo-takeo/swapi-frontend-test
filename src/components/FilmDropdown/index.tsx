f766d4e17806ed85b8c2342fa1d282ca1f97e8ad
Eduardo Takeo <eduardo.higashi.dev@gmail.com>
2021-08-16 22:58:17 -0300

    Create FilmDropdown component

---
+++ src/components/FilmDropdown/index.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { useState } from "react";
+import { IFilm } from "../../interfaces/Film";
+
+import styles from "./styles.module.scss";
+
+interface FilmDropdownProps {
+  film: IFilm;
+}
+
+function FilmDropdown({ film }: FilmDropdownProps) {
+  const [isDetailsVisible, setIsDetailsVisible] = useState<boolean>(false);
+
+  return (
+    <div
+      className={styles.dropdownContainer}
+      onClick={() => setIsDetailsVisible(!isDetailsVisible)}
+    >
+      <div className={styles.filmContainer}>
+        <h1>{film.title}</h1>
+        <p>{film.opening_crawl}</p>
+      </div>
+      {isDetailsVisible && (
+        <div className={styles.detailsContainer}>
+          <h2>Episode {film.episode_id}</h2>
+          <span>
+            <strong>Title:</strong> {film.title}
+          </span>
+          <span>
+            <strong>Opening:</strong> {film.opening_crawl}
+          </span>
+          <span>
+            <strong>Release Date:</strong> {film.release_date}
+          </span>
+          <span>
+            <strong>Director:</strong> {film.director}
+          </span>
+          <span>
+            <strong>Producer:</strong> {film.producer}
+          </span>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default FilmDropdown;
--- src/components/FilmsShowcase/index.tsx
+++ src/components/FilmsShowcase/index.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { IFilm } from "../../interfaces/Film";
+import FilmDropdown from "../FilmDropdown";
 
 interface FilmsShowcaseProps {
   filmsList: IFilm[];
 }
 
 function FilmsShowcase({ filmsList }: FilmsShowcaseProps) {
-  return <div>List of films</div>;
+  return (
+    <section>
+      {filmsList?.map((film) => (
+        <FilmDropdown film={film} />
+      ))}
+    </section>
+  );
 }
 
 export default FilmsShowcase;
